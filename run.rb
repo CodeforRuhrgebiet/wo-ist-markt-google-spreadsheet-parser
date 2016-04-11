c0171a33e5d83a75806cd94a8c63eab9f618fca3
Martin Schurig <martin@schurig.pw>
2016-04-11 12:52:39 +0200

    init commit

---
+++ run.rb
@@ -0,0 +1,28 @@
+require 'csv'
+require 'json'
+
+csv_file = CSV.read("./wochenmaerkte.csv.tsv", { col_sep: "\t" })
+
+features = []
+
+csv_file.each do |row|
+  if row[6] && row[5]
+    feature = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [row[6], row[5]]
+      },
+      properties: {
+        title: row[2],
+        location: row[2],
+        opening_hours: row[7]
+      }
+    }
+
+    features.push(feature)
+  end
+end
+
+data = {type: 'FeatureCollection', features: features}
+File.open("./essen.geojson", 'w') { |file| file.print data.to_json }
